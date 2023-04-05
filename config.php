10041ba21a7f964a7b860f57b5dc62b13a0f11e1
awk <abkhlaf1@gmail.com>
2023-04-05 10:11:11 +0200

    add marss

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+
+    'author'      => "AWK",
+    'title'       => "awkcodes",
+    'link'        => "https://awk.codeberg.page",
+    'description' => "tech oriented, random blog"
+);
+?>
